fc5481770dd20b36a1300631406447c2178be17f
shar3579 <sharmila091104@gmail.com>
2024-09-08 03:26:53 +0530

    Changed App.tsx and Graph.tsx to produce the desired outputs

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -52,10 +52,10 @@ class Graph extends Component<IProps, {}> {
             elem.setAttribute("row_pivots", '["timestamp"]');
             elem.setAttribute("columns", '["top_ask_price"]');
             elem.setAttribute("aggregates",
-                `{"stock":"distinct_count",
+                `{"stock":"distinct count",
                 "top_ask_price":"avg",
                 "top_bid_price":"avg",
-                "timestamp":"distinct_count"}`
+                "timestamp":"distinct count"}`
         );
       // Add more Perspective configurations here.
       elem.load(this.table);
